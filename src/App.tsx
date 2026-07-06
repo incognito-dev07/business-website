0277464e07c8e3adffd166bf78d6c011d5fd3731
incognito-dev07 <osasanolusola2007@gmail.com>
2026-07-06 20:42:04 +0100

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout'
+import Navigation from './components/Navigation'
+import Footer from './components/Footer'
 import Home from './pages/Home'
 import Projects from './pages/Projects'
 import Contact from './pages/Contact'
@@ -7,11 +8,15 @@ import Contact from './pages/Contact'
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout><Home /></Layout>} />
-        <Route path="/projects" element={<Layout><Projects /></Layout>} />
-        <Route path="/contact" element={<Layout><Contact /></Layout>} />
-      </Routes>
+      <Navigation />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+      <Footer />
     </BrowserRouter>
   )
 }
